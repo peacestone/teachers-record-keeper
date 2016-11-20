68052f86ca87fb37cc1fb23eb140ae3f21150661
Baruch Steinmetz <boruchsteinmetz@outlook.com>
2016-11-20 01:39:01 +0000

    updated model associations

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,3 +1,5 @@
 class Subject < ActiveRecord::Base
   belongs_to :teacher
+  has_many :tests
+  has_many :students, through: :tests
 end
--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,3 +1,4 @@
 class Teacher < ActiveRecord::Base
   has_many :subjects
+  has_many :students, through: :subjects
 end
