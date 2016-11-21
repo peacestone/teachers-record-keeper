efe91fb19f2b57867e5f91881433a28c986a89a2
Baruch Steinmetz <boruchsteinmetz@outlook.com>
2016-11-21 02:22:03 +0000

    added teacher view and edit

--- app/controllers/teacher_controller.rb
+++ app/controllers/teacher_controller.rb
@@ -1,4 +1,16 @@
 class TeacherController < ApplicationController
 
-  
+  get "/teachers/:id" do
+    @teacher = Teacher.find_by(id: params[:id])
+    @subjects = @teacher.subjects if @teacher
+    erb :"teachers/show_teacher"
+  end
+
+  get "/teachers/:id/edit" do
+    #binding.pry
+    @teacher = Teacher.find_by(id: params[:id])
+    @subjects = Subject.all
+    @students = Student.all
+    erb :"teachers/edit_teacher"
+  end
 end
--- config.ru
+++ config.ru
@@ -2,4 +2,5 @@ require "./config/environment.rb"
 
 use SubjectsController
 use TestController
+use TeacherController
 run ApplicationController
